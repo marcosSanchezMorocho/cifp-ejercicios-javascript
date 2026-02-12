098a450133c6c8af04cee4f32494757535e4299b
MarcosDaw <marcos7sm9@gmail.com>
2026-02-12 17:58:55 +0100

    Bucles y formularios, saca archivos de carpetas

--- Variables/Ej10/main.js
+++
@@ -1,5 +0,0 @@
-alert("Programa de resta")
-let n1 = window.prompt("Introduzca el primer número");
-let n2 = window.prompt("Introduzca el segundo número");
-
-alert(`${n1} - ${n2} = ${n1-n2}`);
--- Variables/Ej11/main.js
+++
@@ -1,6 +0,0 @@
-alert("Programa de resta y multiplicación")
-let n1 = window.prompt("Introduzca el primer número");
-let n2 = window.prompt("Introduzca el segundo número");
-
-alert(`${n1} - ${n2} = ${n1-n2}`);
-alert(`${n1} x ${n2} = ${n1*n2}`);
